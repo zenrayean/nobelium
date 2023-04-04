5192ab59036ac7d59951b26ea25e54d1d521efb0
zenrayean <78266537+zenrayean@users.noreply.github.com>
2023-04-04 16:57:33 +0800

    Update Scripts.js
    change ga to umami

--- components/Scripts.js
+++ components/Scripts.js
@@ -15,17 +15,7 @@ const Scripts = () => {
       )}
       {BLOG.analytics && BLOG.analytics.provider === 'ga' && (
         <>
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${BLOG.analytics.gaConfig.measurementId}`}
-          />
-          <Script strategy="lazyOnload" id="ga">
-            {`window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${BLOG.analytics.gaConfig.measurementId}', {
-                page_path: window.location.pathname,
-              });`}
-          </Script>
+<script async defer data-website-id="1c5a5b0d-871e-461f-a37d-9c8dd739d15f" src="https://web.gpio.ink/umami.js"></script>
         </>
       )}
     </>
